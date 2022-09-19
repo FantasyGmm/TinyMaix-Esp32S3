fb99a8ecafc6f1b795401850555303e98642460d
FantasyGmm <16450052+FantasyGmm@users.noreply.github.com>
2022-09-19 12:24:17 +0800

    Initial commit

---
+++ main/dsps_dotproduct_main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "cifar.h"
+#include "mbnet128.h"
+#include "mnist.h"
+#include "vww96.h"
+void app_main()
+{
+//	test_cifar(0,NULL);
+	test_mbnet128(0,NULL);
+//	test_mnist(0,NULL);
+//	test_vww96(0,NULL);
+}
